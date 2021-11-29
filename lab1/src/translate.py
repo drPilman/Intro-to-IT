db6e493da20b4a1ed0b6b6461560977a36495062
drPilman <doktorpilman@gmail.com>
2021-11-29 18:38:53 +0300

    solve utf-8 in json post request issue

--- lab1/src/translate.py
+++ lab1/src/translate.py
@@ -9,12 +9,12 @@ params = (('version', '2018-05-01'),)
 url = os.environ['API_URL']
 
 
-def get(text: str, lg: str) -> str:
+def get(text: str, lg: str):
     result = ""
     if text:
         lg_from = 'ru' if lg == 'en' else 'en'
         data = f'{{"text": "{text}", "model_id":"{lg_from}-{lg}"}}'
-        response = requests.post(url, headers=headers, params=params, data=data, auth=('apikey', api_key))
+        response = requests.post(url, headers=headers, params=params, data=data.encode('utf-8'), auth=('apikey', api_key))
         try:
             result = response.json()['translations'][0]['translation']
         except:
